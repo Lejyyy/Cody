b20daa1cdd05c7891b8938018ab2b4cbb8eed041
Jorys Djemili <jorys.dje69@gmail.com>
2026-03-03 16:17:35 +0100

    mmm

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = current_user.projects.order(created_at: :desc).includes(:chat)
+    @projects = Project.all.order(created_at: :desc)
   end
 
   def show
