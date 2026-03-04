901d02c7e2e9d0b9e2917842eba188cac8da08d5
Amandyne DESFONDS <amandyne.desfonds@gmail.com>
2026-03-04 10:18:07 +0100

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,12 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all.order(created_at: :desc)
+    @projects = current_user.projects.order(created_at: :desc)
   end
 
   def show
     @project = current_user.projects.find(params[:id])
     @chat = current_user.chats.find_by(project: @project)
-    @messages = @chat&.messages&.order(:created_at)
+    @messages = @chat&.messages&.order(:created_at) || []
     @message = Message.new
   end
 end
