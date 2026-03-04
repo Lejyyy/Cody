7296c872de91f293695434a64325303b010100d7
Amandyne DESFONDS <amandyne.desfonds@gmail.com>
2026-03-04 10:27:06 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  devise_for :users
 
   resources :users, only: [:show]
 
   resources :projects, only: [:index, :show] do
-    resources :chat, only: [:show, :create]
+    resources :chats, only: [:create]
   end
-
-  devise_for :users
 
   resources :chats, only: [:destroy] do
     resources :messages, only: [:create]
