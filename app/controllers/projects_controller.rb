175d421842b5ef4fdcc56fd1af20a5d11f745cf5
Amandyne DESFONDS <amandyne.desfonds@gmail.com>
2026-03-04 11:21:07 +0100

    Update projects_controller.rb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,26 @@ class ProjectsController < ApplicationController
   def show
     @project = current_user.projects.find(params[:id])
     @chat = current_user.chats.find_by(project: @project)
-    @messages = @chat&.messages&.order(:created_at) || []
+    @messages = @chat&.messages&.order(:created_at)
     @message = Message.new
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = current_user.projects.build(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: "Projet créé."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :short_description, :content, :category, :duration, :level_project, :stack)
   end
 end
